9baa4f28e3804725a7446d2be3970c68ec4970f8
tommylin <sky003428@gmail.com>
2022-10-26 13:53:10 +0800

    feat: 改寫重複數字

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 
+const answerLength = 4;
 let gameTimes = 10;
-let answerLength = 4;
 
-const answerArr = new Array(answerLength);
-for (let i = 0; i < answerArr.length; i++) {
-    answerArr[i] = parseInt(Math.random() * 10);
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let answer = "";
+
+for (let i = 0; i < answerLength; ++i) {
+    const randIndex = Math.floor(Math.random() * (10 - i));
+    answer += arr.splice(randIndex, 1)[0];
 }
-const answer = answerArr.join("")
+
 console.log(`答案是:(${answer})`);
 
 rl.on("line", (input) => {
-    if (input.length != answerLength || Array.from(input, Number).includes(NaN)) {
-        console.log("格式錯誤");
-        return
+    if (new Set(input).size !== answerLength) {
+        console.log("輸入長度錯誤或數字重複");
+        return;
+    } else if (Array.from(input, Number).includes(NaN)) {
+        console.log("包含非法字元");
+        return;
     }
 
     let a = 0;
     let b = 0;
-    const compareArr = [...answerArr];
-    const numArr = Array.from(input, Number);
     gameTimes--;
 
-
-    for (let i = 0; i < answerLength; i++) {
-        if (numArr[i] === compareArr[i]) {
-            a++;
-            compareArr[i] = -1;
-            numArr[i] = -2;
-        }
-    }
-
-    for (let i = 0; i < answerLength; i++) {
-        const compareIndex = compareArr.indexOf(numArr[i]);
+    for (let i = 0; i < answerLength; ++i) {
+        const compareIndex = answer.indexOf(input[i]);
+        console.log(answer[i], input[i], compareIndex);
         if (compareIndex >= 0) {
-            b++;
-            compareArr[compareIndex] = -3;
+            if (compareIndex == i) {
+                ++a;
+            } else {
+                ++b;
+            }
         }
     }
 
@@ -53,10 +52,9 @@ rl.on("line", (input) => {
             rl.close();
         }
     }
-
 });
 
-rl.on('close', () => {
+rl.on("close", () => {
     console.log("Goodbye!");
     process.exit(0);
 });
